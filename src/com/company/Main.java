a90d832df68d515fee49f347aae935998a25f703
PhillipsOlagunju <phillipsolagunju21@gmail.com>
2020-03-20 14:55:02 -0500

    My change to the create file methon

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,117 +1,160 @@
 package com.company;
-import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
 public class Main{
-    static final String[] name = new String[]{"John", "Kate", "Sam", "Kevin", "Paul", "Maddison", "Cooper", "Smith", "Chris", "Tom"};
+
+    // *Phillips*
+    // static final String[] NAME = new String[]{"John", "Kate", "Sam", "Kevin", "Paul", "Maddison", "Cooper", "Smith", "Chris", "Tom"};
+    static final String[] NAMES = {"John", "Kate", "Sam", "Kevin", "Paul", "Maddison", "Cooper", "Smith", "Chris", "Tom"};
+    static final String FILENAME = "generatedData.csv";
+    static final int NUM_OF_ROWS  = 3325;
+    static Scanner console = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
 
         // Generate a file of 3325 Student data
-        File generatedData = createfile(name);
+        //*  I see that I do not have to create a variable for the generated File.
+
+        createfile(NAMES);
+
+        // *Phillips* After you make whatever changes to createFile(), push your code to your repo
+
 
         // Put every name in the file created in an Array
-        String[] names = nameArray(generatedData);
+//        String[] names = nameArray(generatedData);
 
         // put the number of each name in an Array using only Arrays
-        int[] numberOfNames = nameNumber(name, names);
-
-        // find the number of times each name appears
-        Map<String, Integer> nameFrequency = numberOfDifferentNames(name, numberOfNames);
-
-        //Transfer the names from the Array to an ArrayList
-        ArrayList<String> studentNames = arrayToList(names);
-
-        //Sort the names in ascending order
-        Collections.sort(studentNames);
-
-        //function check if an id has a duplicate
-        System.out.println(checkDuplicateID(99890));
-
-        //Create an ArrayList that contains Students creates from the generated file
-        ArrayList<Student> studentlist = createStudentList(generatedData);
-
-        //Remove every other duplicate IDs from the ArrayList of Students
-        ArrayList<Student> newStudentList = removeDuplicate(studentlist);
-
-        //Map of Student names to their frequencies
-        countDifferentNames(studentNames) ;
-
-        // Remove duplicate IDs and create an ArrayList of students directly from the generated file
-        ArrayList<Student> uniqueStudentList = removeDuplicateID(generatedData);
-
-        //Sort the ArrayList of Students in ascending order of the student IDs
-        Collections.sort(uniqueStudentList, Student::compareTo);
-
-        // reverse the ordered student ArrayList into a stack
-        Stack<Student> reversedStudent = reverseStudentList(newStudentList);
-
-        // reverse the ordered student ArrayList using a deque
-        Deque<Student> dequeStudent = reverseStudentWithDeque(newStudentList);
+//        int[] numberOfNames = nameNumber(NAMES, names);
+//
+//        // find the number of times each name appears
+//        Map<String, Integer> nameFrequency = numberOfDifferentNames(NAMES, numberOfNames);
+//
+//        //Transfer the names from the Array to an ArrayList
+//        ArrayList<String> studentNames = arrayToList(names);
+//
+//        //Sort the names in ascending order
+//        Collections.sort(studentNames);
+//
+//        //function check if an id has a duplicate
+//        System.out.println(checkDuplicateID(99890));
+//
+//        //Create an ArrayList that contains Students creates from the generated file
+////        ArrayList<Student> studentlist = createStudentList(generatedData);
+//
+//        //Remove every other duplicate IDs from the ArrayList of Students
+//        ArrayList<Student> newStudentList = removeDuplicate(studentlist);
+//
+//        //Map of Student names to their frequencies
+//        countDifferentNames(studentNames) ;
+//
+//        // Remove duplicate IDs and create an ArrayList of students directly from the generated file
+////        ArrayList<Student> uniqueStudentList = removeDuplicateID(generatedData);
+//
+//        //Sort the ArrayList of Students in ascending order of the student IDs
+//        Collections.sort(uniqueStudentList, Student::compareTo);
+//
+//        // reverse the ordered student ArrayList into a stack
+//        Stack<Student> reversedStudent = reverseStudentList(newStudentList);
+//
+//        // reverse the ordered student ArrayList using a deque
+//        Deque<Student> dequeStudent = reverseStudentWithDeque(newStudentList);
 
 
 
     }
 
-    static File createfile(String[] names) throws IOException {
+    // *Phillips* I'm confused by this method signature (the header of your method/function).
+    // To create a file, shouldn't you use the user's filename rather than your favorite filename?
+    static void createfile(String[] names) throws IOException {
+        System.out.print("Enter  set seed: ");
+        int seed = console.nextInt();
 
         Random rand = new Random();
-        rand.setSeed(5);
-        File file = new File("generatedData.csv");
 
-        PrintWriter writer = null;
-        writer = new PrintWriter(file);
-        writer.write("id,name,gpa\n");
+        // *Phillips* Maybe your user doesn't want to use 5 as the seed.
+        rand.setSeed(seed);
 
-        for(int i = 0; i < 3325; i++) {
+        // *Phillips* You need documentation on this method because you're creating a CSV
+        // but the specs don't say to do this.
+//        File file = new File("generatedData.csv");
 
+        // *Phillips*  This is terrible code.  Here's how it should be written.
+        try(PrintWriter writer = new PrintWriter(new File(FILENAME))) {
+            writer.write("id,name,gpa\n");
+            for (int i = 0; i < NUM_OF_ROWS; i++) {
+                // randomly generate an id in 90_000..99_999
+                int id = rand.nextInt(10_000) + 90_000;
 
-            if (i < 3324) {
-                int index = rand.nextInt(names.length);
-                String name = names[index];
+                // randomly generate a first name
+                String name = NAMES[rand.nextInt(NAMES.length)];
 
+                // randomly generate a gpa
+                // it's not so obvious how to do this since rand.nextDouble gives values in [0..1]
+                int lowerBound = 0;
+                int upperBound = 4;
+                double gpa = lowerBound + rand.nextDouble() * (upperBound - lowerBound);  // think about why this works
 
-                int id;
-                do {
-                    id = rand.nextInt(99999 + 1);
-                } while (id < 90000);
-
-
-                double gpa = 0;
-                do {
-                    gpa = rand.nextDouble();
-                } while (gpa > 4);
-
-
-                writer.write(String.format("%s,%s,%s\n", Integer.toString(id), name, gpa));
-
-
-            }else{
-                int index = rand.nextInt(names.length);
-                String name = names[index];
-
-
-                int id;
-                do {
-                    id = rand.nextInt(99999 + 1);
-                } while (id < 90000);
-                double gpa = 0;
-
-
-                do {
-                    gpa = rand.nextDouble();
-                } while (gpa > 4);
-
-
-                writer.write(String.format("%s,%s,%s", Integer.toString(id), name, gpa));
+                // *Phillips* Now you write to the file
+                writer.write(Integer.toString(id)+","+name+","+Double.toString(gpa)+"\n");
             }
+        } catch(FileNotFoundException e) {
+            e.getStackTrace();
         }
-        writer.close();
 
-        return file;
+//        PrintWriter writer = null;
+//        writer = new PrintWriter(file);
+//        writer.write("id,name,gpa\n");
+//
+//        for(int i = 0; i < 3325; i++) {
+//
+//
+//            if (i < 3324) {
+//                int index = rand.nextInt(names.length);
+//                String name = names[index];
+//
+//
+//                int id;
+//                do {
+//                    id = rand.nextInt(99999 + 1);
+//                } while (id < 90000);
+//
+//
+//                double gpa = 0;
+//                do {
+//                    gpa = rand.nextDouble();
+//                } while (gpa > 4);
+//
+//
+//                writer.write(String.format("%s,%s,%s\n", Integer.toString(id), name, gpa));
+//
+//
+//            }else{
+//                int index = rand.nextInt(names.length);
+//                String name = names[index];
+//
+//
+//                int id;
+//                do {
+//                    id = rand.nextInt(99999 + 1);
+//                } while (id < 90000);
+//                double gpa = 0;
+//
+//
+//                do {
+//                    gpa = rand.nextDouble();
+//                } while (gpa > 4);
+//
+//
+//                writer.write(String.format("%s,%s,%s", Integer.toString(id), name, gpa));
+//            }
+//        }
+//        writer.close();
+
     }
-    static String[] nameArray(File file) throws FileNotFoundException {
+    static String[] nameArray() throws FileNotFoundException {
 
+        File file = new File(FILENAME);
         String[] bannerNames = new String[3325];
         Scanner fileScan = null;
         fileScan = new Scanner(file);
@@ -123,10 +166,11 @@ public class Main{
             bannerNames[i] = parts[1];
             i++;
         }
-    return bannerNames;
+        return bannerNames;
     }
+
     static int[] nameNumber(String[] actualNames, String[] nameArray){
-        int[] numberNames = new int[10];
+        int[] numberNames = new int[actualNames.length];  // *Phillips* how do you know this should be 10?
         for(int i = 0; i < actualNames.length; i++){
             int total = 0;
             for(int j = 0; j < 3325; j++){
@@ -161,7 +205,7 @@ public class Main{
 
     static boolean checkDuplicateID(int id) throws FileNotFoundException {
         Scanner fileScan = null;
-        File file = new File("generatedData.csv");
+        File file = new File(FILENAME);
         fileScan = new Scanner(file);
         ArrayList<String> idArray = new ArrayList<>();
         ArrayList<Integer> duplicateIdsArr = new ArrayList<>();
@@ -184,7 +228,7 @@ public class Main{
         return false;
     }
 
-        static ArrayList<Student> createStudentList(File file) throws FileNotFoundException {
+    static ArrayList<Student> createStudentList(File file) throws FileNotFoundException {
         ArrayList<Student> studentList = new ArrayList<>();
         Scanner fileScan = null;
         fileScan = new Scanner(file);
@@ -196,8 +240,8 @@ public class Main{
             studentList.add(student);
         }
         return studentList;
-        }
-        static ArrayList<Student> removeDuplicate(ArrayList<Student> studentList){
+    }
+    static ArrayList<Student> removeDuplicate(ArrayList<Student> studentList){
         ArrayList<Integer> checkDuplicate = new ArrayList<>();
         for(int i = 0; i < studentList.size(); i++){
             Student student = studentList.get(i);
@@ -209,8 +253,8 @@ public class Main{
         }
 
         return studentList;
-        }
-        static ArrayList<Student> removeDuplicateID(File file) throws FileNotFoundException {
+    }
+    static ArrayList<Student> removeDuplicateID(File file) throws FileNotFoundException {
         Scanner fileScan = null;
         fileScan = new Scanner(file);
         ArrayList<String> id = new ArrayList<>();
@@ -227,44 +271,45 @@ public class Main{
         }
 
         return studentList;
+    }
+
+
+    static Map<String, Integer> numberOfDifferentNames(String[] nameArray, int[] numberOfNames){
+        Map<String, Integer> namesMapToFrequency= new HashMap<>();
+        for(int i = 0; i < numberOfNames.length; i++){
+            namesMapToFrequency.put(nameArray[i], numberOfNames[i]);
+        }
+        return namesMapToFrequency;
+    }
+
+    static ArrayList<String> arrayToList(String[] names){
+        ArrayList<String> studentNames = new ArrayList<>();
+        for(int i = 0; i < names.length; i++){
+            studentNames.add(names[i]);
         }
 
+        return studentNames;
+    }
 
-        static Map<String, Integer> numberOfDifferentNames(String[] nameArray, int[] numberOfNames){
-            Map<String, Integer> namesMapToFrequency= new HashMap<>();
-            for(int i = 0; i < numberOfNames.length; i++){
-                namesMapToFrequency.put(nameArray[i], numberOfNames[i]);
-            }
-            return namesMapToFrequency;
+    static Stack<Student> reverseStudentList(ArrayList<Student> students){
+        Stack<Student> studentStack = new Stack<>();
+        for(Student student : students){
+            studentStack.push(student);
         }
 
-        static ArrayList<String> arrayToList(String[] names){
-            ArrayList<String> studentNames = new ArrayList<>();
-            for(int i = 0; i < names.length; i++){
-                studentNames.add(names[i]);
-            }
+        return studentStack;
+    }
 
-            return studentNames;
+    static Deque<Student> reverseStudentListUsingStack(ArrayList<Student> students){
+        Deque<Student> studentDeque = new MyStack();
+        return studentDeque;
+    }
+
+    static Deque<Student> reverseStudentWithDeque(ArrayList<Student> students){
+        Deque<Student> studentDeque = new LinkedList<>();
+        for(Student student : students){
+            studentDeque.push(student);
         }
-
-        static Stack<Student> reverseStudentList(ArrayList<Student> students){
-            Stack<Student> studentStack = new Stack<>();
-            for(Student student : students){
-               studentStack.push(student);
-            }
-
-            return studentStack;
-        }
-
-        static Deque<Student> reverseStudentListUsingStack(ArrayList<Student> students){
-            Deque<Student> studentDeque = new MyStack();
-        }
-
-        static Deque<Student> reverseStudentWithDeque(ArrayList<Student> students){
-            Deque<Student> studentDeque = new LinkedList<>();
-            for(Student student : students){
-                studentDeque.push(student);
-            }
-            return studentDeque;
-        }
+        return studentDeque;
+    }
 } // Class Main
